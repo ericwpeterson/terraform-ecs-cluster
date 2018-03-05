c6d30b45ef19c32f5175679f2faee9a47e6fe05f
Eric <erockp1@gmail.com>
2018-03-05 10:21:55 -0600

    first commit.

---
+++ service.tf
@@ -0,0 +1,20 @@
+resource "aws_ecs_service" "test-ecs-service" {
+  	name            = "test-ecs-service"
+  	iam_role        = "${aws_iam_role.ecs-service-role.name}"
+  	cluster         = "${aws_ecs_cluster.test-ecs-cluster.id}"
+  	task_definition = "${aws_ecs_task_definition.wordpress.family}:${max("${aws_ecs_task_definition.wordpress.revision}", "${data.aws_ecs_task_definition.wordpress.revision}")}"
+  	desired_count   = 1
+    depends_on = ["null_resource.alb_exists"]
+
+  	load_balancer {
+    	target_group_arn  = "${aws_alb_target_group.ecs-target-group.arn}"
+    	container_port    = 80
+    	container_name    = "wordpress"
+	}
+}
+
+resource "null_resource" "alb_exists" {
+  triggers {
+    alb_name = "${aws_alb.ecs-load-balancer.arn}"
+  }
+}
---
+++ task-definition.tf
@@ -0,0 +1,24 @@
+data "aws_ecs_task_definition" "wordpress" {
+  task_definition = "${aws_ecs_task_definition.wordpress.family}"
+}
+
+resource "aws_ecs_task_definition" "wordpress" {
+    family                = "hello_world"
+    container_definitions = <<DEFINITION
+[
+  {
+    "name": "wordpress",
+    "image": "eric1123/docker-nodejs-demo",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "memory": 256,
+    "cpu": 1
+  }
+]
+DEFINITION
+}
