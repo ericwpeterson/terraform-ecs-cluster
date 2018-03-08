fef60f78cb1467710bf4f0de14389c009277c217
Eric <erockp1@gmail.com>
2018-03-08 14:32:55 -0600

    added s3 backend

---
+++ terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+ backend  "s3" {
+ encrypt = true
+ bucket = "eric-peterson-terraform-remote-state-storage-s3"
+ dynamodb_table = "terraform-state-lock-dynamo"
+ region = "us-west-2"
+ key = "ecs-cluster-state/terraform.tfstate"
+ }
+}
